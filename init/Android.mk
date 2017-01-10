b75f45d485657267586ccabdc71ac0a84525acd9
Adrian DC <radian.dc@gmail.com>
2017-01-10 22:00:15 +0100

    init: Makefile: Provide an init.real target to create init.real
     * When BOARD_SONY_INIT_SYMLINK is set to true,
        the init.real generation for init_sony is done
        using the AOSP way for sonyxperiadev projects,
        if not, the regular custombootimg has to be used
    
     * Original implementation for sonyxperiadev
        by Dave Hylands <dhylands@gmail.com>,
        560e05fb6416954f8d3ca0a1d05482634a32a635
    
    Change-Id: I43d6497696eceea6921ee6a63431e67fc3dc11c2

--- init/Android.mk
+++ init/Android.mk
@@ -60,3 +60,34 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_CLANG := true
 
 include $(BUILD_EXECUTABLE)
+
+# Build init.real if requested
+ifeq ($(BOARD_SONY_INIT_SYMLINK),true)
+
+# Abort if the device is not handled by init_sony
+ifeq (,$(findstring DEV_BLOCK_FOTA_NUM,$(BOARD_SONY_INIT_FLAGS)))
+$(error device-sony-common-init: DEV_BLOCK_FOTA_NUM missing in "$(TARGET_DEVICE)", platform "$(PRODUCT_PLATFORM)", with '$(BOARD_SONY_INIT_FLAGS)')
+endif
+
+# Init overrides for init_sony
+root_init      := $(TARGET_ROOT_OUT)/init
+root_init_real := $(TARGET_ROOT_OUT)/init.real
+
+# If /init is a file and not a symlink then rename it to /init.real
+# and make /init be a symlink to /sbin/init_sony (which will execute
+# /init.real, if appropriate.
+$(root_init_real): $(root_init) $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/utilities/init_sony
+	cp $(PRODUCT_OUT)/utilities/toybox $(TARGET_ROOT_OUT)/sbin/toybox_init
+	cp $(PRODUCT_OUT)/utilities/keycheck $(TARGET_ROOT_OUT)/sbin/keycheck
+	cp $(PRODUCT_OUT)/utilities/init_sony $(TARGET_ROOT_OUT)/sbin/init_sony
+	$(hide) if [ ! -L $(root_init) ]; then \
+	  echo "/init $(root_init) isn't a symlink"; \
+	  mv $(root_init) $(root_init_real); \
+	  ln -s sbin/init_sony $(root_init); \
+	else \
+	  echo "/init $(root_init) is already a symlink"; \
+	fi
+
+ALL_DEFAULT_INSTALLED_MODULES += $(root_init_real)
+
+endif
